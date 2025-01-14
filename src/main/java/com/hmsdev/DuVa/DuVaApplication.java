44ce90e1514bc1c68b6be3497768e4a035103e14
hms10dev <hms10dev@gmail.com>
2025-01-14 10:16:25 -0500

    spring project added

---
+++ src/main/java/com/hmsdev/DuVa/DuVaApplication.java
@@ -0,0 +1,13 @@
+package com.hmsdev.DuVa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DuVaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DuVaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hmsdev/DuVa/DuVaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hmsdev.DuVa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DuVaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
